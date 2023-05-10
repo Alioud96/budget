2b9044c452a0f50bfa803391d887e585eec1d5ee
Alioud96 <127201566+Alioud96@users.noreply.github.com>
2023-05-10 14:06:20 +0000

    Update projet.py

--- projet.py
+++ projet.py
@@ -1,6 +1,4 @@
-
 import sqlite3
-
 # # Creation de la base de donnees de budgetDB
 
 conn = sqlite3.connect('budggetDB.db')
@@ -8,47 +6,230 @@ cur = conn.cursor()
 
 conn.commit()
 
-
-# Création de la table "depenses"
-conn.execute('''CREATE TABLE depenses
-             (id INTEGER PRIMARY KEY AUTOINCREMENT,
-             categorie_depense numeric,
-             nourriture numeric,
-             transport);''')
-                         
-             
- # Insertion de données dans la table "depenses"
-conn.execute("INSERT INTO depenses (categorie_depense) VALUES ('nourriture')")
-conn.execute("INSERT INTO depenses (categorie_depense) VALUES ('transport')")
-
-# Enregistrement des modifications
-conn.commit()
-
-# Exécution d'une requête pour récupérer les données insérées
-cursor = conn.execute("SELECT id, categorie_depense FROM depenses")
-for row in cursor:
-    print(f"id = {row[0]}, categorie_depense = {row[1]}")
-
-# Fermeture de la connexion à la base de données
-conn.close()
-
-def table_revenu():
-    req2="CREATE TABLE revenu(id INTEGER PRIMARY KEY, categorie revenu, business numeric )"
-    cur.execute(req2) 
+def create_depense():
+    req="CREATE TABLE depense(id INTEGER PRIMARY KEY, montant numeric, categorie varchar)"
+    cur.execute(req) 
     conn.commit()
-    
-table_revenu()
+create_depense()
 
 
-def ajouter_revenu():
-    categorie revenu =int(input("veuiller donner la categorie de votre revenu svp "))
-    business =int(input("veuiller donner le revenu de votre business svp "))
+def ajouter_depense():
+    montant =int(input("veuiller donner votre montant svp "))
+    categorie =input("veuiller donner votre type de categorie svp")
     
-    req2="INSERT INTO revenu(categorie revenu,bussiness) Values (?,?)"
-    cur.execute(req2,(categorie revenu,business))
+    req1="INSERT INTO depense(montant, categorie) Values (?,?)"
+    cur.execute(req1,(montant, categorie))
     conn.commit() 
     
-ajouter_categorie() 
+ajouter_depense() 
+
+
+def table_revenu():
+    req2="CREATE TABLE revenu(id INTEGER PRIMARY KEY, montant numeric, categorie varchar)"
+    cur.execute(req2) 
+    conn.commit()   
+table_revenu()
+
+def ajouter_revenu():
+    montant =int(input("veuiller donner votre montant svp "))
+    categorie =input("veuiller donner votre type de categorie svp ")
+    
+    req2="INSERT INTO revenu(montant,categorie) Values (?,?)"
+    cur.execute(req2,(montant, categorie))
+    conn.commit()
+
+ajouter_revenu()
+
+# calcul du depense total
+montant =int(input("veuiller donner votre montant svp "))
+categorie =input("veuiller donner votre type de categorie svp ")
+
+# calcul de la somme des attributs des depenses
+somme = montant + categorie
+
+# affichage de la somme des depenses
+print("La somme de", montant, "et", categorie, "est :", somme)
+
+
+# calcul du revenu total
+montant =int(input("veuiller donner votre montant svp "))
+categorie =input("veuiller donner votre type de categorie svp ")
+
+# calcul de la somme des attributs des revenus
+somme = montant + categorie
+
+# affichage de la somme des revenus
+print("La somme de", montant, "et", categorie, "est :", somme)
+
+# calcul de l'ecart
+if total_depense < total_revenu:
+    ecart = total_revenu-total_depense
+    print("l'ecart entre les depenses et les  revenus est :"+str(ecart)+"fcfa")
+elif total_revenu < total_depense:
+    ecart = total_depense-total_revenu
+    print("l'ecart entre les depenses est :"+str(ecart)+"fcfa")
+else:
+    print("pas d'ecart entre les depenses et les revenues")
+
+# print("l'ecart entre vos depense et revenus est estimee a :" + str(ecart) + "fcfa")
+
+conn.commit()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 # calcul du depense total
